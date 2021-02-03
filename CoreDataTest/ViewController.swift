a619362093c829867fc15cd58e6ae652d0b54892
장태현 <danielchang27@gmail.com>
2021-02-04 08:36:38 +0900

    Initial Commit

---
+++ CoreDataTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataTest
+//
+//  Created by moonkyoochoi on 2021/02/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
